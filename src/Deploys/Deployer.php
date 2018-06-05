971974f0214d8e01e387aae044d91c62bd909025
Bruno Goncalves <bugotech@gmail.com>
2018-06-05 16:17:36 -0300

    ajustes de estrutura add deploy

--- src/Deploys/Deployer.php
+++ src/Deploys/Deployer.php
@@ -119,7 +119,7 @@ class Deployer
      */
     public function resolve($file)
     {
-        $class = Str::studly(implode('_', array_slice(explode('_', $file), 4)));
+        $class = Str::studly($file);
 
         return new $class($this);
     }
