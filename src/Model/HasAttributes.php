696030b38a0747eae51ff471342e23ee13580ef3
Bruno Goncalves <bugotech@gmail.com>
2018-06-13 10:20:06 -0300

    set raw attributes

--- src/Model/HasAttributes.php
+++ src/Model/HasAttributes.php
@@ -155,6 +155,24 @@ trait HasAttributes
     }
 
     /**
+     * Set the array of model attributes. No checking is done.
+     *
+     * @param  array  $attributes
+     * @param  bool  $sync
+     * @return $this
+     */
+    public function setRawAttributes(array $attributes, $sync = false)
+    {
+        $this->attributes = $attributes;
+
+        if ($sync) {
+            $this->syncOriginal();
+        }
+
+        return $this;
+    }
+
+    /**
      * Remove a attribute.
      *
      * @param $key
